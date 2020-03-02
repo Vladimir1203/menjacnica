21227ff9219d56a6e074c48d37601e95e6b5be65
Vladimir1203 <vladimir12934@gmail.com>
2020-03-02 10:06:38 +0100

    Napravljena klasa Valuta sa svojim atributima.

---
+++ Menjacnica/src/valuta/Valuta.java
@@ -0,0 +1,51 @@
+package valuta;
+import java.util.Date;
+
+public class Valuta {
+	
+	private String naziv;
+	private String skraceniNaziv;
+	private Date datum;
+	private double prodajni;
+	private double srednji;
+	private double kupovni;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public Date getDatum() {
+		return datum;
+	}
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	
+	
+}
